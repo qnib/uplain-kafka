3697048ac600ea137c9e3f1e8bdd1086d158afec
Christian Kniep <christian@qnib.org>
2017-09-05 10:41:10 +0200

    remove advertised.listeners

--- opt/qnib/entry/20-kafka.sh
+++ opt/qnib/entry/20-kafka.sh
@@ -8,9 +8,6 @@ else
     KAFKA_BROKER_ID=0
 fi
 
-if [[ -z ${ADVERTISED_LISTENERS} ]];then
-    ADVERTISED_LISTENERS=${HOSTNAME}:${KAFKA_PORT}
-fi
 
 mkdir -p /opt/kafka/config
 echo "[II] Write config using: KAFKA_PORT=${KAFKA_PORT}, ZK_SERVERS=${ZK_SERVERS}, KAFKA_BROKER_ID=${KAFKA_BROKER_ID}, INTER_BROKER_PROTOCOL_VERSION=${INTER_BROKER_PROTOCOL_VERSION}, LOG_MESSAGE_FORMAT_VERSION=${LOG_MESSAGE_FORMAT_VERSION}"
@@ -18,7 +15,11 @@ cat /opt/qnib/kafka/conf/server.properties \
    | sed -e "s/KAFKA_PORT/${KAFKA_PORT}/" \
          -e "s/ZK_SERVERS/${ZK_SERVERS}/" \
          -e "s/KAFKA_BROKER_ID/${KAFKA_BROKER_ID}/" \
-         -e "s/ADVERTISED_LISTENERS/${ADVERTISED_LISTENERS}/" \
          -e "s/INTER_BROKER_PROTOCOL_VERSION/${INTER_BROKER_PROTOCOL_VERSION}/" \
          -e "s/LOG_MESSAGE_FORMAT_VERSION/${LOG_MESSAGE_FORMAT_VERSION}/" \
 > /opt/kafka/config/server.properties
+
+if [[ "X${ADVERTISED_LISTENERS}" != "X" ]];then
+    echo "[!!] Not yet implemented in configure script. Please create sed command in /opt/qnib/entry/20-kafka.sh"
+    exit 1
+fi
